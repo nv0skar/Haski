d666c47ef9066511663202e53a6ce99b90d8b779
ItsTheGuy <36568572+ItsTheGuy@users.noreply.github.com>
2022-05-28 21:15:52 +0200

    Added license

--- src/trader.rs
+++ src/trader.rs
@@ -1,10 +1,22 @@
-// Haski - Oscar
-// The use of this is restricted to only the authors
-
-use wyhash::WyHash;
+// Haski
+// Copyright (C) 2022 ItsTheGuy
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 pub mod heart {
     use std::{hash::Hasher, process::exit};
+    use wyhash::WyHash;
 
     #[derive(Debug)]
     pub enum Actions {
@@ -75,7 +87,7 @@ pub mod heart {
                 patternValueDerivation.push(valueDerivation)
             }
 
-            let mut hash = super::WyHash::with_seed(0);
+            let mut hash = WyHash::with_seed(0);
             hash.write(&patternValueDerivation);
             let calculatedHash = hash.finish();
 
@@ -204,7 +216,7 @@ pub mod heart {
                 patternValueDerivation.push(valueDerivation)
             }
 
-            let mut hash = super::WyHash::with_seed(0);
+            let mut hash = WyHash::with_seed(0);
             hash.write(&patternValueDerivation);
             let calculatedHash = hash.finish();
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,29 @@
-// Haski - Oscar
-// The use of this is restricted to only the authors
+// Haski
+// Copyright (C) 2022 ItsTheGuy
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 pub mod show {
     use colored::Colorize;
 
-    pub fn print(origin: &str, data: &String) { println!("{} {}", format!("({})", origin).bold().yellow(), data) }
-    pub fn printError(origin: &str, data: &String) { println!("{} {}", format!("(ERROR at {})", origin).bold().red(), data) }
-    pub fn printTitle(data: &str) { println!("{}", format!("--- {} ---", data).bright_white()) }
+    pub fn print(origin: &str, data: &String) {
+        println!("{} {}", format!("({})", origin).bold().yellow(), data)
+    }
+    pub fn printError(origin: &str, data: &String) {
+        println!("{} {}", format!("(ERROR at {})", origin).bold().red(), data)
+    }
+    pub fn printTitle(data: &str) {
+        println!("{}", format!("--- {} ---", data).bright_white())
+    }
 }
